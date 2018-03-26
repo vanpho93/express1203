490b045d73e4a5145f9971d62226fecddee3fe13
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-26 16:10:15 +0700

    Word

--- Word.js
+++ Word.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/reactnative1203')
 .then(() => console.log('Database connected.'))
 .catch((error) => console.log('Cannot connect database.', error))
+
+const wordSchema = new mongoose.Schema({
+    en: { type: String, trim: true, unique: true },
+    vn: { type: String, trim: true, unique: true },
+    isMemorized: { type: Boolean, default: false }
+});
+
+const Word = mongoose.model('Word', wordSchema);
