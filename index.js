b51e87d7729531f00c51ee1d0c2d1fa275c870e9
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-26 16:48:45 +0700

    xong

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/word', (req, res) => {
     // Show ra tat ca words
     Word.find({})
     .then(words => res.send({ success: true, words }))
+    .catch(error => res.send({ success: false, error: error.message }));
+});
+
+app.delete('/word/:_id', (req, res) => {
+    Word.findByIdAndRemove(req.params._id)
+    .then(word => {
+        if (!word) throw new Error('Cannot find word.');
+        res.send({ success: true, word });
+    })
     .catch(error => res.send({ success: false, error: error.message }))
 });
 
