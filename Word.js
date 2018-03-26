bc0d4cf649738a004a0b0729c034605155b3bf7a
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-26 16:36:54 +0700

    xong

--- Word.js
+++ Word.js
@@ -5,9 +5,50 @@ mongoose.connect('mongodb://localhost/reactnative1203')
 .catch((error) => console.log('Cannot connect database.', error))
 
 const wordSchema = new mongoose.Schema({
-    en: { type: String, trim: true, unique: true },
-    vn: { type: String, trim: true, unique: true },
-    isMemorized: { type: Boolean, default: false }
+    en: { type: String, trim: true, unique: true, required: true },
+    vn: { type: String, trim: true, required: true },
+    isMemorized: { type: Boolean, default: false, required: true }
 });
 
 const Word = mongoose.model('Word', wordSchema);
+
+// const word = new Word({ en: 'Four', vn: 'Bon' });
+// word.save()
+// .then(w => console.log(w))
+// .catch(error => console.log(error));
+
+// Word.find({ en: 'Five' })
+// .then(words => console.log(words))
+// .catch(error => console.log(error));
+
+// Word.findOne({ en: 'Five' })
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.findById('5ab8ba82348d8f5640808522')
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.findByIdAndRemove('5ab8ba82348d8f5640808522')
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.findOneAndRemove({ en: 'Five' })
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.remove({})
+// .then(x => console.log(x))
+// .catch(error => console.log(error));
+
+// Word.findByIdAndUpdate('5ab8bdfe4b39155734618af7', { en: 'five' }, { new: true })
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.findOneAndUpdate({ en: 'Four' }, { vn: 'BON' }, { new: true })
+// .then(word => console.log(word))
+// .catch(error => console.log(error));
+
+// Word.updateMany({}, { isMemorized: false }, { new: true })
+// .then(x => console.log(x))
+// .catch(error => console.log(error));
