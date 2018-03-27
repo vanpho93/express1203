ed1d988938f6f60f90a8d0041e012291f2e50138
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-27 15:19:10 +0700

    post

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { Word } = require('./Word');
 
+const parser = bodyParser.urlencoded({ extended: false });
+
 const app = express();
+app.use(parser);
 
 app.get('/word', (req, res) => {
     // Show ra tat ca words
@@ -17,6 +21,14 @@ app.delete('/word/:_id', (req, res) => {
         res.send({ success: true, word });
     })
     .catch(error => res.send({ success: false, error: error.message }))
+});
+
+app.post('/word', (req, res) => {
+    const { en, vn } = req.body;
+    const word = new Word({ en, vn });
+    word.save()
+    .then(word => res.send({ success: true, word }))
+    .catch(error => res.send({ success: false, error: error.message }));
 });
 
 app.listen(3000, () => console.log('Server started.'));
