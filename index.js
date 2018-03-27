0876737df7cb09e2c679d37db6565d955bbd55b4
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-27 15:27:45 +0700

    put

--- index.js
+++ index.js
@@ -10,25 +10,35 @@ app.use(parser);
 app.get('/word', (req, res) => {
     // Show ra tat ca words
     Word.find({})
-    .then(words => res.send({ success: true, words }))
-    .catch(error => res.send({ success: false, error: error.message }));
+        .then(words => res.send({ success: true, words }))
+        .catch(error => res.send({ success: false, error: error.message }));
 });
 
 app.delete('/word/:_id', (req, res) => {
     Word.findByIdAndRemove(req.params._id)
-    .then(word => {
-        if (!word) throw new Error('Cannot find word.');
-        res.send({ success: true, word });
-    })
-    .catch(error => res.send({ success: false, error: error.message }))
+        .then(word => {
+            if (!word) throw new Error('Cannot find word.');
+            res.send({ success: true, word });
+        })
+        .catch(error => res.send({ success: false, error: error.message }))
 });
 
 app.post('/word', (req, res) => {
     const { en, vn } = req.body;
     const word = new Word({ en, vn });
     word.save()
-    .then(word => res.send({ success: true, word }))
-    .catch(error => res.send({ success: false, error: error.message }));
+        .then(word => res.send({ success: true, word }))
+        .catch(error => res.send({ success: false, error: error.message }));
+});
+
+app.put('/word/:_id', (req, res) => {
+    const { isMemorized } = req.body;
+    Word.findByIdAndUpdate(req.params._id, { isMemorized }, { new: true })
+        .then(word => {
+            if (!word) throw new Error('Cannot find word.');
+            res.send({ success: true, word });
+        })
+        .catch(error => res.send({ success: false, error: error.message }));
 });
 
 app.listen(3000, () => console.log('Server started.'));
